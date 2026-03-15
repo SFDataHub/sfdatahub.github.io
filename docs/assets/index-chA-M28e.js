81f565033d6a5e7d7544af96aa888a25aac0cdfd
Dein Name <deine@mailadresse.tld>
2026-03-15 10:20:57 +0100

    docs

---
+++ docs/assets/index-chA-M28e.js
@@ -0,0 +1,11 @@
+const e=`\uFEFF---
+title: "Dungeon Pause open XP Calculator"
+category: "Progression"
+status: "up_to_date"
+gallery: []
+---
+The recommended "To" values after clicking the RESET BUTTON are set with a Berserker with Mine 25 and no heavy shrooming as a base value, so you can add a few levels on top if you are a heavy shroomer or have a stronger PvE Class.
+Continious Loop of Idols clear until stage 21 is only achievable with the "Potion Trick" for STR / INT Classes! (Using STR+INT+DEX Potions simultaneously)
+
+[[embed:dungeon-pause-open-xp-calculator]]
+`;export{e as default};
